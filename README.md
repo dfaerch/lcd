0231b04ab326aa0b6754decf313fe115830a32b0
Dan Faerch <seqcheck.dan@hacker.dk>
2016-10-02 13:46:09 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,84 @@
+# LocateChdir aka. "lcd"
+
+Program that uses the "locate" command and some cleverness, to find the directory the user wants to "cd" to. Simply search by running the command with one or more (ordered) string arguments to look for in the path.
+
+Examples
+--------
+
+Chdir to Downloads inside users home dir
+
+    $ lcd hom Down
+
+Or if we just specify "Do" we hit Documents 
+
+    $ lcd Do
+    /home/dan/Documents
+
+Everytime you re-run the command, it'll pick the next likely candidate, based on current-dir:
+
+    $ lcd Do
+    /home/dan/Downloads
+
+
+
+
+Prerequisites
+-------------
+
+You need perl, which is usually on most Linux's & Unix's by default.
+
+You need to have locate. This is also somewhat default. "updatedb" must have been run at least once. Check your "locate" installation by running "locate home/|head". Either you get path names or you get an error. If missing, then you must install it. For debian/ubuntu you would do:
+
+ $ sudo apt-get install mlocate
+ $ sudo updatedb
+
+Let updatedb run to completion (can take a minute or two).
+
+Installation
+------------
+
+
+Place the program somewhere. Eg. /opt/LocateChdir/. Then add this to your shells environment.
+
+    lcd () { eval "`/opt/LocateChdir/lcd.pl $*`";}
+
+
+This can be put in eg. ~/.bashrc. On Ubuntu it makes sense to put it in ~/.bash_aliases, if you like. Then restart your shell start lcd'ing.
+
+
+Troubleshooting
+----------------
+
+### lcd to a newly created directory, doesnt work.
+ lcd uses `locate` - remember to run `updatedb`, or locate wont know about your new directory.
+
+
+Notes about the search algorithm
+------------------
+
+ * Lowercase search string = case-insensitive search. 
+ * Dirs inaccessable to user, is filtered out. (otherwise she would just get an access denied error)
+ * Shortest paths are visisted first, unless a "boosted" result supersedes it.
+ * Boosts includes:
+     * Users home directory. "lcd Documents" will go to users own /home/user/Documents, before it will go to /home/another_user/Documents.
+     * Hidden directories (dirs starting with a dot) gets downvoted slightly.
+     * Last directory. "lcd apache2" will rather go to "/etc/apache2/ than to "/apache2/etc" because LAST dir matches searchstring best.
+     * If last directory perfectly matches search string, it will massively boost this result. Eg. "lcd Go" will rather go to "/home/user/development/Go/" than
+       "/home/user/GoLang_docs/"
+
+
+TODO / Ideas
+------------
+
+lcd could be sped up by exporting the "locate" data into a our own database, keeping only directories. This should massively reduce the size of the data to search
+each time. Then we could check the timestamp of the locatedb and see if it was time to repopulate our own.
+
+Another option could be to simply cache results, since users likely jump between a fixed set of dirs, most of the time. Again we could check timestamp of locatedb to expire cache.
+
+License
+-------
+
+GPL 3.0 or any later version of GPL.
+
+Copyright 2016 - Dan Faerch
+
---
+++ lcd.pl
@@ -0,0 +1,187 @@
+#!/usr/bin/perl -w
+#
+# lcd - the Locate Change Dir command. Copyright 2016 - Dan Faerch.
+#
+
+use warnings;
+use strict;
+use Cwd;
+
+# TODO/IDEA: augment list of dirs with a scan of bash/zsh/* history.
+my $LOCATE_BIN="locate";
+my $DEBUG = 0;
+
+
+my @search = @ARGV;
+if (!@search ) {
+    print  STDERR "Must supply a search string\n";
+    print ".";
+    exit 1;
+}
+
+
+# STUB
+#-----
+# Is this an lcd - \d+? Step back in history
+if (join(" ",@search) =~ m/^- (\d+)/) {
+    # We cant currently, but this space is reserved for that
+    print STDERR "currently not implemented\n";
+    
+    # TODO: Keep an LCDHIST env-var, with n dirs seperated by : (like path). 
+    # split here and pick the item chosen by user.
+    
+    exit;
+}
+#-----
+
+
+
+# Make regex
+@search = map { quotemeta } @search;
+
+my $search = "(".join('.*?', @search);
+$search .= ')[^\/]*$';
+$search =~ s/'/\'/;
+
+my $is_lowercase = (lc $search eq $search);
+my $i = "";
+$i = "(?i)" if $is_lowercase;
+my $search_qr = qr/$i$search/;
+
+
+
+# Run locate
+my $cmd = $LOCATE_BIN;
+$cmd .= " -i" if $is_lowercase;
+$cmd .= " --regex '$search'";
+
+print STDERR $cmd."\n" if $DEBUG;
+
+
+# Filter once more, since locate doesnt do it right
+my @files = grep {m/$search_qr/} `$cmd`;
+
+# Sort using our own sort function
+my @sdirs = multi_sort(@files);
+
+my $index = 0;
+my $cwd = getcwd();
+
+# See if we are currently standing in a matched directory and assume we want to go the the next one
+if (my @match = grep { $sdirs[$_] eq $cwd."\n" } 0..$#sdirs) {
+    my $i = $match[0]+1;
+    if ($i <= @sdirs) {
+        $index = $i % @sdirs;
+    }
+}
+
+
+my $result;
+# Pick the first dir, after index. (because locate also prints files)
+for (my $i = $index;$i<@sdirs;$i++) {
+    my $e = $sdirs[$i];
+    chomp($e);
+    if ( -d $e && -x $e) {
+        $result = $e;
+        last;
+    }
+}
+
+if (!defined $result || $result eq $cwd) {
+
+    print STDERR "Nothing found\n";
+    
+} else {
+
+    print STDERR $result."\n";
+    $result =~ s/'/'\\''/g;
+    print "cd '".$result."'\n";
+
+}
+
+#----------------
+
+sub multi_sort {
+    my @files = @_;
+    
+    my $home = $ENV{HOME};
+    my $home_depth =()= $home =~ m%/%g;    
+    
+    my @temp;
+    foreach (@files) {
+        my %h = ();
+
+        # Store original
+        $h{org} = $_;
+        # Length
+        $h{len} = length($_);
+
+        # DEPTH
+        #----
+        # Count slashes
+        $h{depth} =()= $_ =~ m%/%g;
+        # Subtract home depth, to prioritize things in users home
+        if (m%^\Q$home\E/%) {
+            $h{depth} -= $home_depth 
+        } elsif ( m%^/home/% ) {
+            #add 1 depth, if path is within another home dir
+            $h{depth} += 1 if (m%/\.%);
+        }
+
+
+        #add 1 depth, if path is within a hidden dir (eg .config)
+        $h{depth} += 1 if (m%/\.%);
+        
+
+        # Length after match
+        # ------- 
+        # Sliding window search looking for the match closest to the end and store how far from the end it is
+        my $tmp = $_;
+        my $mpos;
+        my $epos = 0;
+        while ($tmp =~ m/$search_qr/) {
+            # store length - end-pos of last match
+            $mpos = 0 if !defined $mpos;
+            $mpos += $-[1];
+
+            # Chop away anything before match, +1, and try again (sliding window match)
+            $tmp = substr($tmp,$-[1]+1);
+            $epos = length($1)+1; # add one for the chopped of letter
+        }
+
+        if (defined $mpos) {
+            $h{len_after_match} = $h{len} - ($mpos+$epos+1);
+        } else {
+            $h{len_after_match} = $h{len};
+            print STDERR "Error - NO MATCH? $search_qr - $_\n";
+        }
+
+        
+        # Perfect last dir
+        # ---------
+        # If searchstring perfectly matches the last dir, sort higher (eg. search apache, sorts "apache" higher than "apache2")
+        m%/([^/]+)$%;
+        my $last_part = "/".$1;
+        
+        if ( $last_part =~ m%$search_qr% ) {
+            $h{perfect_last_dir} = 1;
+        } else {
+            $h{perfect_last_dir} = 0;
+        }
+
+        push @temp,\%h;
+        
+    }
+    
+
+    #print Dumper(\@temp);
+    
+     return
+             map  { $_->{org} } 
+              sort { 
+                 $a->{perfect_last_dir} <=> $b->{perfect_last_dir}
+                 || $a->{depth} <=> $b->{depth}
+                 || $a->{len_after_match} <=> $b->{len_after_match} 
+                 || $a->{len} <=> $b->{len} 
+                 } @temp;    
+}
